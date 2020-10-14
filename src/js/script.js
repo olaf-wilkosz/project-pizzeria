28b76160083fa54d2b7070ac39817564cc2badf8
Olaf Wilkosz <olaf.wilkosz@gmail.com>
2020-10-14 20:48:11 +0200

    Add showing and hiding cart on click

--- src/js/script.js
+++ src/js/script.js
@@ -328,6 +328,43 @@
     }
   }
 
+  class Cart {
+    constructor(element) {
+      const thisCart = this;
+
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+      thisCart.initActions();
+
+      console.log('new Cart:', thisCart);
+    }
+
+    getElements(element) {
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions() {
+      const thisCart = this;
+
+      console.log('thisCart.dom.wrapper:', thisCart.dom.wrapper);
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function (event) {
+        console.log('clicked');
+        event.preventDefault();
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+        console.log('thisCart.dom.wrapper:', thisCart.dom.wrapper);
+      });
+
+    }
+  }
+
   const app = {
     initMenu: function () {
       const thisApp = this;
@@ -345,16 +382,24 @@
       thisApp.data = dataSource;
     },
 
+    initCart: function () {
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
+
     init: function () {
       const thisApp = this;
-      console.log('*** App starting ***');
-      console.log('thisApp:', thisApp);
-      console.log('classNames:', classNames);
-      console.log('settings:', settings);
-      console.log('templates:', templates);
+      // console.log('*** App starting ***');
+      // console.log('thisApp:', thisApp);
+      // console.log('classNames:', classNames);
+      // console.log('settings:', settings);
+      // console.log('templates:', templates);
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
 
   };
